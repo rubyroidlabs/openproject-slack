eed174c2173ec2cd4bd2a6ac2dd2d5201e319cf3
Yakav Krasnov <yasha.krasnov@gmail.com>
2018-05-03 15:18:43 +0300

    make notify post request synchronous

--- lib/open_project/slack/notifier.rb
+++ lib/open_project/slack/notifier.rb
@@ -1,3 +1,5 @@
+require 'httpclient'
+
 class OpenProject::Slack::Notifier
   def speak(msg, channel, attachment=nil, url=nil)
     url = Setting.plugin_openproject_slack['slack_url'] if url.blank?
@@ -26,7 +28,7 @@ class OpenProject::Slack::Notifier
       client = HTTPClient.new
       client.ssl_config.cert_store.set_default_paths
       client.ssl_config.ssl_version = :auto
-      client.post_async url, { payload: params.to_json }
+      client.post url, { payload: params.to_json }
     rescue Exception => e
       Rails.logger.warn("cannot connect to #{url}")
       Rails.logger.warn(e)
